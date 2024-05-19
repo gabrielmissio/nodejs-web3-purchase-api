34351a70c8dbadea47a18a92bece25f65b6d0123
gabrielmissio <gabrielmissio.dev@gmail.com>
2024-05-18 23:09:58 -0300

    feat: add `Purchase` smartcontract and setup Hardhat

---
+++ blockchain/hardhat.config.js
@@ -0,0 +1,19 @@
+require('@dotenvx/dotenvx').config({ path: '../.env' })
+require('@nomicfoundation/hardhat-ethers')
+
+module.exports = {
+  solidity: {
+    version: '0.8.4',
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  },
+  networks: {
+    localhost: {
+      url: process.env.RCP_URL,
+      accounts: [process.env.ACCOUNT_KEY],
+    },
+  },
+}
