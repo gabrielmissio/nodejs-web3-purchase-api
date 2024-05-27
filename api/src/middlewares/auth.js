6924eee2837ebb60417b6b22d3e433254e78f079
gabrielmissio <gabrielmissio.dev@gmail.com>
2024-05-26 22:53:38 -0300

    feat: add auth middleware

---
+++ api/src/middlewares/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+  const token = req.header('Authorization')
+
+  if (!token) return res.status(400).json({
+    error: 'Missing \'Authorization\' header',
+  })
+    
+  try {
+    jwt.verify(
+      token.replace('Bearer ', ''),
+      process.env.AUTH_JWT_SECRET,
+    )
+  
+    next()
+  } catch {
+    return res.status(401).json({
+      error: 'Unauthorized',
+    })
+  }
+}
--- api/src/routes.js
+++ api/src/routes.js
@@ -1,49 +1,57 @@
 const router = require('express').Router()
-const validator = require('./middlewares/validator')
+const authMiddleware = require('./middlewares/auth')
+const validatorMiddleware = require('./middlewares/validator')
 const authController = require('./presentation/controllers/auth-controller')
 const authValidations = require('./presentation/validations/auth-validations')
 const purchaseController = require('./presentation/controllers/purchase-controller')
 const purchaseValidations = require('./presentation/validations/purchase-validations')
 
+// ** Open endpoints **
 router.get('/health', (req, res) => res.status(200).json({ status: 'ok' }))
 
-// Auth endpoints
 router.post(
   '/auth/login',
-  validator(authValidations.login),
+  validatorMiddleware(authValidations.login),
   authController.login,
 )
+
+router.get(
+  '/products',
+  validatorMiddleware(purchaseValidations.listProducts),
+  purchaseController.listProducts,
+)
+
+// ** Protected endpoints **
+router.use(authMiddleware)
+
 router.post(
   '/auth/signup',
-  validator(authValidations.signup),
+  validatorMiddleware(authValidations.signup),
   authController.signup,
 )
+
 router.post(
   '/auth/change-password',
-  validator(authValidations.changePassword),
+  validatorMiddleware(authValidations.changePassword),
   authController.changePassword,
 )
 
-// Purchase endpoints
 router.post(
   '/purchases/publish',
-  validator(purchaseValidations.publishPurchase),
+  validatorMiddleware(purchaseValidations.publishPurchase),
   purchaseController.publishPurchase,
 )
+
 router.post(
   '/purchases/abort',
-  validator(purchaseValidations.abortPurchase),
+  validatorMiddleware(purchaseValidations.abortPurchase),
   purchaseController.abortPurchase,
 )
+
 router.post(
   '/purchases/settle-funds',
-  validator(purchaseValidations.settleFunds),
+  validatorMiddleware(purchaseValidations.settleFunds),
   purchaseController.settleFunds,
-)
-router.get(
-  '/products',
-  validator(purchaseValidations.listProducts),
-  purchaseController.listProducts,
 )
 
 module.exports = router
