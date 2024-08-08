88694fe2673ba49042df4ad651ed35d461646fb1
gabrielmissio <gabmissil@gmail.com>
2024-08-08 00:26:27 -0300

    feat: add deploy frontend script

--- tools/stacks/deploy-frontend.sh
+++ tools/stacks/deploy-frontend.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
 # Check if at least the correct number of arguments is provided
-if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 <appName> <region> [stage]"
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 <appName> <region> <stage>"
     exit 1
 fi
 
 # Assign arguments to variables
 appName=$1
 region=$2
-
-# Check if stage is provided, otherwise set it to an empty string
 stage=$3
-if [ -z "$stage" ]; then
-    stage=""
-fi
 
-STACK_NAME="${appName}-S3DeploymentBucket"
+STACK_NAME="${appName}-StaticWebsite-${stage}"
+
+# TODO: adjust stack output key if necessary (it is ...)
 S3_BUCKET=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --region "$region" --query "Stacks[0].Outputs[?OutputKey=='S3BucketName'].OutputValue" --output text)
 
 if [ -z "$S3_BUCKET" ]; then
@@ -24,17 +21,11 @@ if [ -z "$S3_BUCKET" ]; then
     exit 1
 fi
 
-# Adjust the S3 path based on whether the stage is provided
-if [ -z "$stage" ]; then
-    S3_PATH="s3://$S3_BUCKET/abis"
-else
-    S3_PATH="s3://$S3_BUCKET/abis/$stage"
-fi
-
+S3_PATH="s3://$S3_BUCKET"
 echo "Syncing ABIs to $S3_PATH"
 
 # Sync the local directory to the S3 bucket
-aws s3 sync ./blockchain/abis $S3_PATH
+aws s3 sync ./frontent/public $S3_PATH
 
 # Check if the sync command was successful
 if [ $? -eq 0 ]; then
