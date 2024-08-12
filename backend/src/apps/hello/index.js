3fe52e60861fa30a41a0d5607d6f28aa9bd53f40
gabrielmissio <gabmissil@gmail.com>
2024-08-11 23:57:12 -0300

    feat: add rpc proxy lambda

--- backend/src/apps/hello/index.js
+++ backend/src/apps/hello/index.js
@@ -4,13 +4,22 @@ const provider = new JsonRpcProvider(process.env.RCP_PROVIDER_PRIVATE_IP)
 
 module.exports.handler = async (event) => {
   try {
-    console.log('event', event)
+    console.log('event', event) // API Gateway event
+    // get request body
+    const body = JSON.parse(event.body)
+    console.log('body', body)
+
     const { chainId } = await provider.getNetwork()
     console.log('chainId', chainId)
 
-    return true
+    // send body to provider "as a proxy"
+    const result = await provider.send(body.method, body.params)
+    console.log('result', result)
+
+    return result
   } catch (error) {
     console.error('error', error)
     return false
   }
 }
+//module.exports.handler({}) // test
